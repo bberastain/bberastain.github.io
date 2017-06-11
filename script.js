5b8dd0089fc847367318cf2592221e4fe4634502
Bruno Berastain <bberastain@gmail.com>
2017-06-11 03:09:10 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function showExperience() {
+    var x = document.getElementById('experience');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    }
+    else {
+        x.style.display = 'block';
+    }
+}
+function showProcess() {
+    var x = document.getElementById('process');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    }
+    else {
+        x.style.display = 'block';
+    }
+}
+function showExpectations() {
+    var x = document.getElementById('expectations');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    }
+    else {
+        x.style.display = 'block';
+    }
+}
+function showBooking() {
+    var x = document.getElementById('booking');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    }
+    else {
+        x.style.display = 'none';
+    }
+}
+function showResources() {
+    var x = document.getElementById('resources');
+    if (x.style.display === 'block') {
+        x.style.display = 'none';
+    }
+    else {
+        x.style.display = 'block';
+    }
+}
